9b3f793a1372d333bc45ab9b0d74c19a70eb663d
Matthew Versfeld <Mattversfeld@gmail.com>
2024-02-18 11:39:33 +0200

    added main page text as well as styling

--- MyPropCo/src/components/MainBodyText.jsx
+++ MyPropCo/src/components/MainBodyText.jsx
@@ -1,7 +1,27 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
 export default function MainBodyText() {
 
 
     return (
-        <div>MainBodyText goes here</div>
+        <div className='main__container'>
+            <h1 className='main__title'>Lorem ipsum dolor sit amet</h1>
+            <p className='main__paragraph'>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant morbi tristique senectus et netus et. Urna nec tincidunt praesent semper. Ac tortor vitae purus faucibus ornare suspendisse sed. Lacus sed viverra tellus in hac habitasse. Iaculis at erat pellentesque adipiscing commodo elit. Fringilla urna porttitor rhoncus dolor purus non. Facilisis gravida neque convallis a cras semper auctor neque vitae. Porttitor lacus luctus accumsan tortor posuere ac ut.</p>
+            <Button
+                className='main__button'
+                sx={{
+                    bgcolor: '#FC495D',
+                    fontSize: 10,
+                    fontFamily: 'Open Sans',
+                    height: 30,
+                    width: 95,
+                    marginTop: 3
+                }}
+                variant="contained">Read more</Button>
+        </div>
     )
 }
+
+
+
