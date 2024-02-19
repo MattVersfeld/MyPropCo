88f9897c59cdc584c7b46a624503df41cd2e838d
Matthew Versfeld <Mattversfeld@gmail.com>
2024-02-19 08:46:43 +0200

    added bonus question to app.jsx to be run in console

--- MyPropCo/src/App.jsx
+++ MyPropCo/src/App.jsx
@@ -10,6 +10,27 @@ import './App.css'
 
 export default function App() {
 
+  /**
+   * Bonus question: feel free to add any amount in the console log below
+   */
+  function propData(num) {
+    let list = []
+
+    for (let i = 0; i <= num - 1; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        list.push('PropData')
+      } else if (i % 3 === 0) {
+        list.push('Prop')
+      } else if (i % 5 === 0) {
+        list.push('Data')
+      } else {
+        list.push(i.toString())
+      }
+    }
+    return list
+  }
+
+  console.log(propData(50))
 
   return (
     <>
